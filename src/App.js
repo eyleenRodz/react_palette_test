9d7b754645b81fa68e4b0b23a05df9ceceee5beb
eyleenRodz <eyleen.rodz@gmail.com>
2021-10-16 10:07:49 -0500

    se suben cambios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import { usePalette } from 'react-palette'
 
 function App() {
+  let img = "https://thumbs.dreamstime.com/b/d%C3%ADa-del-medio-ambiente-en-tierra-manos-de-%C3%A1rboles-cultivando-pl%C3%A1ntulas-fondo-verde-bokeh-hembra-sujetando-un-%C3%A1rbol-el-campo-la-130247647.jpg"
+  const { data, loading, error } = usePalette(img)
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +21,12 @@ function App() {
           Learn React
         </a>
       </header>
+      <section>
+        
+      <div style={{ backgroundColor: data.vibrant , color: 'white', }}>
+        Text with the vibrant color
+      </div>
+      </section>
     </div>
   );
 }
